aa82ea46dc4914c708eb61b6cc154958010e470d
Carlos Rosquillas <crosquillas@gmail.com>
2025-08-11 16:16:25 -0700

    feat: Allow vi mode for Powershell

--- tools/pwsh/profile.ps1
+++ tools/pwsh/profile.ps1
@@ -4,6 +4,13 @@ Import-Module Sudo
 # Include local binaries
 $env:Path += ';' + $HOME + '\.local\bin'
 
+# Exit with ctrl+d
+Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+
+# Vim mode
+Set-PSReadlineOption -EditMode vi
+Set-PSReadLineOption -ViModeIndicator Cursor 
+
 # Inlcude all scripts in the config profile.d folder
 $scriptFolder = Join-Path $HOME '.config/powershell/profile.d'
 $scripts = Get-ChildItem -Path $scriptFolder -Filter '*.ps1'
