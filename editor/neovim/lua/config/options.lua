c2e20b1afe7deb9a6792c9dffa789336523a461a
Carlos Rosquillas <crosquillas@gmail.com>
2025-08-12 12:09:33 -0700

    fix: Address LazyHealth issues

--- editor/neovim/lua/config/options.lua
+++ editor/neovim/lua/config/options.lua
@@ -53,6 +53,7 @@ vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python_provider = 0
+vim.g.loaded_python3_provider = 0
 
 -- Set undofile for persistent undo
 vim.o.undofile = true
--- editor/neovim/lua/plugins/mason.lua
+++ editor/neovim/lua/plugins/mason.lua
@@ -1,16 +1,27 @@
 return {
-
   "williamboman/mason.nvim",
   version = "1.11.0",
-  opts = {
-    ensure_installed = {
+  opts = function()
+    -- Base list of packages to install on all systems
+    local ensure_installed = {
       "bash-language-server",
       "css-lsp",
       "html-lsp",
       "eslint-lsp",
+      "markdown-toc",
+      "markdownlint-cli2",
+      "prettier",
       "prettierd",
       "sqlfluff",
-      "checkmake",
-    },
-  },
+    }
+
+    -- Conditionally add 'checkmake' if the OS is not Windows
+    if not vim.fn.has("win32") then
+      table.insert(ensure_installed, "checkmake")
+    end
+
+    return {
+      ensure_installed = ensure_installed,
+    }
+  end,
 }
