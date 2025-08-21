0cca01e05907a91d1d13c518068b2de3e21eb0f2
Carlos Rosquillas <crosquillas@gmail.com>
2025-08-21 09:51:06 -0700

    feat: Import default eza options

--- tools/eza/eza.zsh
+++ tools/eza/eza.zsh
@@ -1,27 +1,23 @@
+
 if (( $+commands[eza] )); then
   typeset enable_autocd=0
-  typeset -ag eza_params
 
-  eza_params=(
-    '--icons' '--group' '--group-directories-first'
-    '--time-style=long-iso' '--color-scale=all'
-  )
+  # Default parameters for eza
+  export _EZA_PARAMS=('--git' '--group' '--group-directories-first' '--time-style=long-iso' '--hyperlinks')
 
-  [[ ! -z $_EZA_PARAMS ]] && eza_params=($_EZA_PARAMS)
-
-  alias ls='eza $eza_params'
-  alias l='eza --git-ignore $eza_params'
-  alias ll='eza --all --header --long $eza_params'
-  alias llm='eza --all --header --long --sort=modified $eza_params'
+  alias ls='eza'
+  alias l='eza --git-ignore'
+  alias ll='eza --all --header --long'
+  alias llm='eza --all --header --long --sort=modified'
   alias la='eza -lbhHigUmuSa'
   alias lx='eza -lbhHigUmuSa@'
-  alias lt='eza --tree $eza_params'
-  alias tree='eza --tree $eza_params'
+  alias lt='eza --tree'
+  alias tree='eza --tree'
 
   [[ "$AUTOCD" = <-> ]] && enable_autocd="$AUTOCD"
   if [[ "$enable_autocd" == "1" ]]; then
     # Function for cd auto list directories
-    →auto-eza() { command eza $eza_params; }
+    →auto-eza() { command eza; }
     typeset -g chpwd_functions
     [[ $chpwd_functions[(r)→auto-eza] == →auto-eza ]] || chpwd_functions=( →auto-eza $chpwd_functions )
   fi
