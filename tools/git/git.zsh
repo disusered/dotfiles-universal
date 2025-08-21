134ef06d9dbef975a0157c34c1df9a05bc1b61e3
Carlos Rosquillas <crosquillas@gmail.com>
2025-08-21 16:16:51 -0700

    chore: Go back to symlinks

--- tools/git/git.zsh
+++ tools/git/git.zsh
@@ -27,3 +27,33 @@ alias gco='git co'
 export GIT_LOG_STYLE_BASIC="%C(magenta bold)%h%C(reset) %C(auto)%d%C(reset) %s"
 export GIT_LOG_STYLE_COMPLEX="%C(magenta bold)%h%C(reset) %C(blue bold)%aN%C(reset) %C(auto)%d%C(reset) %s %C(8)(%cr)%C(reset)"
 export GIT_LOG_STYLE=$GIT_LOG_STYLE_COMPLEX
+
+# Git Log Interactive
+# https://gist.github.com/junegunn/f4fca918e937e6bf5bad
+function gli() {
+  local out shas sha q k
+  while out=$(
+      git l $@ --format=$GIT_LOG_STYLE |
+      fzf --ansi --no-sort --reverse --multi --query="$q" \
+          --print-query --expect=ctrl-d,ctrl-c,ctrl-i,ctrl-r --toggle-sort=\`); do
+    q=$(head -1 <<< "$out")
+    k=$(head -2 <<< "$out" | tail -1)
+    shas=$(sed '1,2d;s/^[^a-z0-9]*//;/^$/d' <<< "$out" | awk '{print $1}')
+    [ -z "$shas" ] && continue
+    if [ "$k" = ctrl-d ]; then
+      nvim fugitive://$(git rev-parse --show-toplevel)/.git//$(git rev-parse $shas)
+    elif [ "$k" = ctrl-i ]; then
+      git rebase --interactive $shas
+      break
+    elif [ "$k" = ctrl-c ]; then
+      echo $shas | tr -d '\n' | clipcopy
+      break
+    elif [ "$k" = ctrl-r ]; then
+      git reset --hard $shas
+    else
+      for sha in $shas; do
+        git show --color=always $sha
+      done
+    fi
+  done
+}
--- tools/ripgrep/fn_rgi.zsh
+++
@@ -1,8 +0,0 @@
-# Search with ripgrep and pipe to delta for a side-by-side view
-# Usage: rgi [rg_flags] <search_term>
-# Example: rgi -C 2 my_search_term
-if [ $# -eq 0 ]; then
-  echo "Usage: rg [rg_flags] <search_term>" >&2
-  return 1
-fi
-$HOMEBREW_PREFIX/bin/rg --json -C 2 "$@" | delta
