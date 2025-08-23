91bbb6780ed25b10063245bb90aa44c02a603057
Carlos Rosquillas <crosquillas@gmail.com>
2025-08-22 22:00:53 -0700

    feat: Add table debug function to neovim

--- editor/neovim/init.lua
+++ editor/neovim/init.lua
@@ -1,4 +1,27 @@
 -- Prepend mise shims to PATH
+-- TODO: Only do this for windows
 vim.env.PATH = vim.env.HOME .. "\\AppData\\Local\\mise\\shims:" .. vim.env.PATH
 
 require("config.lazy")
+
+-- Add lua debug helper
+local function dump(o)
+  if type(o) == "table" then
+    local s = "{ "
+    for k, v in pairs(o) do
+      if type(k) ~= "number" then
+        k = '"' .. k .. '"'
+      end
+      s = s .. "[" .. k .. "] = " .. dump(v) .. ","
+    end
+    return s .. "} "
+  else
+    return tostring(o)
+  end
+end
+
+function _G.dump(o)
+  vim.notify(dump(o), vim.log.levels.INFO, {
+    title = "debug",
+  })
+end
