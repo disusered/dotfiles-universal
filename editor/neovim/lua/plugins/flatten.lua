e3113ff262d5db81fc5bce0df9558dbc0b35896f
Carlos Rosquillas <crosquillas@gmail.com>
2025-08-22 22:07:08 -0700

    feat: Install flatten plugin only on Windows

--- editor/neovim/lua/plugins/flatten.lua
+++ editor/neovim/lua/plugins/flatten.lua
@@ -2,9 +2,11 @@
 return {
   {
     "brianhuster/unnest.nvim",
-
     -- Ensure that it runs first to minimize delay when opening file from terminal
     lazy = false,
     priority = 1001,
+    cond = function()
+      return vim.loop.os_uname().sysname == "Windows_NT"
+    end,
   },
 }
