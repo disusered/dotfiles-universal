2191c00c1e240ef8b618e4aa48730fbc11d0a700
Carlos Rosquillas <crosquillas@gmail.com>
2025-08-22 22:05:59 -0700

    feat: Add improved LSP signature support

--- editor/neovim/lua/plugins/disabled.lua
+++ editor/neovim/lua/plugins/disabled.lua
@@ -1,19 +1,18 @@
 return {
   -- disable bufferline
-  { "akinsho/bufferline.nvim", enabled = false },
+  { "akinsho/bufferline.nvim",     enabled = false },
   -- no automatic pairs
-  { "mini.pairs", enabled = false },
+  { "mini.pairs",                  enabled = false },
   -- no session management
-  { "folke/persistence.nvim", enabled = false },
+  { "folke/persistence.nvim",      enabled = false },
   -- don't use search/replace plugin <leader>sr
-  { "MagicDuck/grug-far.nvim", enabled = false },
+  { "MagicDuck/grug-far.nvim",     enabled = false },
   -- disable tokyonight
-  { "folke/tokyonight.nvim", enabled = false },
+  { "folke/tokyonight.nvim",       enabled = false },
   -- disable UI extras
-  { "folke/noice.nvim", enabled = false },
-  { "folke/edgy.nvim", enabled = false },
+  { "folke/edgy.nvim",             enabled = false },
   -- no file explorer
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   -- disable visual aids on fFtT and /
-  { "folke/flash.nvim", enabled = false },
+  { "folke/flash.nvim",            enabled = false },
 }
