6e1994d1bbc03369ea8368aac04c0a322f2e34c0
Carlos Rosquillas <crosquillas@gmail.com>
2025-08-23 02:34:05 -0700

    fix: Don't overwrite tab for path completion

--- tools/pwsh/profile.ps1
+++ tools/pwsh/profile.ps1
@@ -11,24 +11,6 @@ Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
 Set-PSReadlineOption -EditMode vi
 Set-PSReadLineOption -ViModeIndicator Cursor 
 
-# Accept suggestion with tab
-Set-PSReadLineKeyHandler -Key Tab `
-                         -BriefDescription ForwardCharAndAcceptNextSuggestionWord `
-                         -LongDescription "Move cursor one character to the right in the current editing line and accept the next word in suggestion when it's at the end of current editing line" `
-                         -ScriptBlock {
-    param($key, $arg)
-
-    $line = $null
-    $cursor = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-
-    if ($cursor -lt $line.Length) {
-        [Microsoft.PowerShell.PSConsoleReadLine]::ForwardChar($key, $arg)
-    } else {
-        [Microsoft.PowerShell.PSConsoleReadLine]::AcceptSuggestion($key, $arg)
-    }
-}
-
 # Inlcude all scripts in the config profile.d folder
 $scriptFolder = Join-Path $HOME '.config/powershell/profile.d'
 $scripts = Get-ChildItem -Path $scriptFolder -Filter '*.ps1'
