339659a1e9cd0cff336dc3bff71c827a3217e608
Carlos Rosquillas <crosquillas@gmail.com>
2025-08-23 10:20:13 -0700

    feat: Fix luarocks integration with LazyVim

--- editor/neovim/lua/config/lazy.lua
+++ editor/neovim/lua/config/lazy.lua
@@ -68,6 +68,9 @@ require("lazy").setup({
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
   },                -- automatically check for plugin updates
+  rocks = {
+    enabled = true,
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
