5d471a3e42cf7288dedf4dca555561cf26d287f1
Carlos Rosquillas <crosquillas@gmail.com>
2025-08-22 23:09:13 -0700

    feat: Install wslu

--- editor/neovim/init.lua
+++ editor/neovim/init.lua
@@ -7,6 +7,19 @@ end
 
 require("config.lazy")
 
+-- Override vim.ui.open
+vim.ui.open = function(url, opts)
+  -- Check if wslview is available and if the input is a URL
+  if vim.fn.executable("wslview") == 1 and url:match("^https?://") then
+    -- Use wslview to open the URL in the default Windows browser
+    -- jobstart is used to run the command asynchronously without blocking Neovim
+    vim.fn.jobstart({ "wslview", url }, { detach = true })
+  else
+    -- Fallback to the original function for local files or other cases
+    original_ui_open(url, opts)
+  end
+end
+
 -- Add lua debug helper
 local function dump(o)
   if type(o) == "table" then
