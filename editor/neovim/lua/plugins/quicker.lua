a6e54aa30ff47ab4afe025384982265bb8c9ad4c
Carlos Rosquillas <crosquillas@gmail.com>
2025-08-23 18:00:11 -0700

    feat: Add improvements to Postgres CLI

--- editor/neovim/lua/plugins/quicker.lua
+++ editor/neovim/lua/plugins/quicker.lua
@@ -4,20 +4,21 @@ return {
   ---@module "quicker"
   ---@type quicker.SetupOptions
   opts = {},
+  -- FIXME: keymaps that dont conflict with other plugins
   keys = {
-    {
-      ">",
-      function()
-        require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
-      end,
-      desc = "Expand quickfix context",
-    },
-    {
-      "<",
-      function()
-        require("quicker").collapse()
-      end,
-      desc = "Collapse quickfix context",
-    },
+    -- {
+    --   ">",
+    --   function()
+    --     require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+    --   end,
+    --   desc = "Expand quickfix context",
+    -- },
+    -- {
+    --   "<",
+    --   function()
+    --     require("quicker").collapse()
+    --   end,
+    --   desc = "Collapse quickfix context",
+    -- },
   },
 }
