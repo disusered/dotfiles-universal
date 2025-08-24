cf5bd6275b97ab7c340ddd441beb009313274894
Carlos Rosquillas <crosquillas@gmail.com>
2025-08-24 12:22:34 -0700

    feat: Use bat instead of cat in windows

--- tools/bat/bat.ps1
+++ tools/bat/bat.ps1
@@ -1,15 +1,24 @@
-$Env:BAT_CONFIG_DIR = '~/AppData/Local/bat/'
-$Env:BAT_THEME = 'Catppuccin Mocha'
+# Only rn if bat is installed
+$batCommand = Get-Command bat.exe -ErrorAction SilentlyContinue
+if ($batCommand) {
+    # Set environment variables for bat configuration
+    $Env:BAT_CONFIG_DIR = Join-Path $HOME "AppData/Local/bat/"
+    $Env:BAT_THEME = 'Catppuccin Mocha'
 
-# Use Select-String with -Quiet for efficiency.
-# It returns $true if a match is found, $false otherwise.
-$catppuccinInstalled = bat --list-themes 2>&1 | Select-String -Pattern 'Catppuccin' -Quiet
-if (-not $catppuccinInstalled) {
-    try {
-        # Execute the command to rebuild the cache.
-        bat cache --build
-    } catch {
-        Write-Error $_
+    # Remove existing cat alias
+    Remove-Alias -Name cat -Force
+
+    # See if theme is installed, if not, rebuild cache
+    $catppuccinInstalled = bat --list-themes 2>&1 | Select-String -Pattern 'Catppuccin' -Quiet
+    if (-not $catppuccinInstalled) {
+        try {
+            # Execute the command to rebuild the cache.
+            bat cache --build
+        }
+        catch {
+            Write-Error $_
+        }
     }
-}
 
+    function cat { bat $args }
+}
