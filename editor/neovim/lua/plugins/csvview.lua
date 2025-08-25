981f0d8c959ab01d05119984814228e97a27ef74
Carlos Rosquillas <crosquillas@gmail.com>
2025-08-25 09:35:15 -0700

    feat: Show cursorline when opening CSV view

--- editor/neovim/lua/plugins/csvview.lua
+++ editor/neovim/lua/plugins/csvview.lua
@@ -1,30 +1,46 @@
 return {
   "hat0uma/csvview.nvim",
+  cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+  init = function()
+    -- Create a dedicated augroup to organize our autocommands
+    local csv_augroup = vim.api.nvim_create_augroup("CsvViewCursorline", { clear = true })
+
+    -- When csvview attaches to a buffer, enable cursorline for that window
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "CsvViewAttach",
+      group = csv_augroup,
+      callback = function()
+        vim.wo.cursorline = true
+      end,
+      desc = "Enable cursorline for csvview",
+    })
+
+    -- When csvview detaches, disable cursorline
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "CsvViewDetach",
+      group = csv_augroup,
+      callback = function()
+        vim.wo.cursorline = false
+      end,
+      desc = "Disable cursorline for csvview",
+    })
+  end,
   ---@module "csvview"
   ---@type CsvView.Options
   opts = {
     parser = { comments = { "#", "//" } },
-
+    view = {
+      display_mode = "border",
+    },
     keymaps = {
-      -- Text objects for selecting fields
       textobject_field_inner = { "if", mode = { "o", "x" } },
       textobject_field_outer = { "af", mode = { "o", "x" } },
-      -- Excel-like navigation:
-      -- Use <Tab> and <S-Tab> to move horizontally between fields.
-      -- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
-      -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
       jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
       jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
       jump_next_row = { "<Enter>", mode = { "n", "v" } },
       jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
     },
-
-    view = {
-      display_mode = "border",
-    },
   },
-  cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
-
   keys = function()
     local csvview = require("csvview")
     local wk = require("which-key")
