37730d7820b70f11acaaf5fa707af53af11a22ae
Carlos Rosquillas <crosquillas@gmail.com>
2025-09-09 09:48:24 -0700

    feat(editor): Add color hints to Neovim

---
+++ editor/neovim/lua/plugins/colors.lua
@@ -0,0 +1,89 @@
+return {
+  {
+    "brenoprata10/nvim-highlight-colors",
+    init = function()
+      local Snacks = require("snacks")
+
+      Snacks.toggle
+        .new({
+          name = "Toggle CSV View",
+          get = function()
+            return require("nvim-highlight-colors").is_active()
+          end,
+          set = function(enabled)
+            local highlight = require("nvim-highlight-colors")
+            if enabled then
+              highlight.turnOn()
+            else
+              highlight.turnOff()
+            end
+          end,
+        })
+        :map("<leader>uH")
+    end,
+    opts = {
+      ---Render style
+      ---@usage 'background'|'foreground'|'virtual'
+      render = "virtual",
+
+      ---Set virtual symbol (requires render to be set to 'virtual')
+      virtual_symbol = " ",
+
+      ---Set virtual symbol suffix (defaults to '')
+      virtual_symbol_prefix = "",
+
+      ---Set virtual symbol suffix (defaults to ' ')
+      virtual_symbol_suffix = "",
+
+      ---inline mimics VS Code style
+      ---eol stands for `end of column` - Recommended to set `virtual_symbol_suffix = ''` when used.
+      ---eow stands for `end of word` - Recommended to set `virtual_symbol_prefix = ' ' and virtual_symbol_suffix = ''` when used.
+      virtual_symbol_position = "inline",
+
+      ---Highlight tailwind colors, e.g. 'bg-blue-500'
+      enable_tailwind = false,
+    },
+  },
+  {
+    "Saghen/blink.cmp",
+    opts = {
+      completion = {
+        menu = {
+          draw = {
+            components = {
+              -- customize the drawing of kind icons
+              kind_icon = {
+                text = function(ctx)
+                  -- default kind icon
+                  local icon = ctx.kind_icon
+                  -- if LSP source, check for color derived from documentation
+                  if ctx.item.source_name == "LSP" then
+                    local color_item =
+                      require("nvim-highlight-colors").format(ctx.item.documentation, { kind = ctx.kind })
+                    if color_item and color_item.abbr ~= "" then
+                      icon = color_item.abbr
+                    end
+                  end
+                  return icon .. ctx.icon_gap
+                end,
+                highlight = function(ctx)
+                  -- default highlight group
+                  local highlight = "BlinkCmpKind" .. ctx.kind
+                  -- if LSP source, check for color derived from documentation
+                  if ctx.item.source_name == "LSP" then
+                    local color_item =
+                      require("nvim-highlight-colors").format(ctx.item.documentation, { kind = ctx.kind })
+                    if color_item and color_item.abbr_hl_group then
+                      highlight = color_item.abbr_hl_group
+                    end
+                  end
+                  return highlight
+                end,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
