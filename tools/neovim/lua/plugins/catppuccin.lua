5800f0f84a2fca36de2b4b064f4e6ec4dcea686d
Carlos Rosquillas <crosquillas@gmail.com>
2025-10-23 09:26:43 -0700

    feat(arch): Use semitransparent background for Kitty

---
+++ tools/neovim/lua/plugins/catppuccin.lua
@@ -0,0 +1,10 @@
+return {
+  "catppuccin/nvim",
+  opts = {
+    transparent_background = true,
+    float = {
+      transparent = true, -- enable transparent floating windows
+      solid = false, -- use solid styling for floating windows, see |winborder|
+    },
+  },
+}
