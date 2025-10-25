619385555c25dea52a7ed60f75fa41567b93bcf2
Carlos Rosquillas <crosquillas@gmail.com>
2025-10-24 22:36:42 -0700

    feat(neovim): Add molten for jupyter notebooks

--- editors/neovim/config/options.lua
+++ editors/neovim/config/options.lua
@@ -52,8 +52,8 @@ vim.opt.splitbelow = false
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
-vim.g.loaded_python_provider = 0
-vim.g.loaded_python3_provider = 0
+
+vim.g.python3_host_prog = vim.fn.expand("~/.local/share/virtualenvs/neovim/bin/python3")
 
 -- Set undofile for persistent undo
 vim.o.undofile = true
