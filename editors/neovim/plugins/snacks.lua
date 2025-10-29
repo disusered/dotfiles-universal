625308d3f6c80852be85804247e48b163407b07a
Carlos Rosquillas <crosquillas@gmail.com>
2025-10-29 13:38:52 -0700

    feat(neovim): Configure zen mode without dimming and with wrapping

--- editors/neovim/plugins/snacks.lua
+++ editors/neovim/plugins/snacks.lua
@@ -12,6 +12,35 @@ return {
     bigfile = { notify = true },
     terminal = { enabled = false }, -- do not use snacks terminal
     lazygit = { enabled = false },
+    zen = {
+      enabled = true,
+      toggles = {
+        dim = false,
+        git_signs = false,
+        mini_diff_signs = false,
+        diagnostics = false,
+        inlay_hints = false,
+        wrap = true,
+      },
+      show = {
+        statusline = false,
+        tabline = false,
+      },
+      on_open = function()
+        vim.fn.system("kitten @ set-font-size +2")
+      end,
+      on_close = function()
+        vim.fn.system("kitten @ set-font-size 0")
+      end,
+    },
+    styles = {
+      zen = {
+        width = 100,
+        wo = {
+          colorcolumn = "",
+        },
+      },
+    },
   },
   keys = {
     {
