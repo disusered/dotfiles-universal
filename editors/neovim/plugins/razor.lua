db21a377d03fc24e7c27ee7559b3135d8ebc9cfa
Carlos Rosquillas <crosquillas@gmail.com>
2025-12-30 23:34:44 -0800

    chore(neovim): Update razor lsp and syntax plugin

--- editors/neovim/plugins/razor.lua
+++ editors/neovim/plugins/razor.lua
@@ -16,7 +16,6 @@ return {
       },
       ensure_installed = {
         "roslyn",
-        "rzls",
       },
     },
   },
@@ -24,63 +23,37 @@ return {
     "seblyng/roslyn.nvim",
     ---@module 'roslyn.config'
     ---@type RoslynNvimConfig
-    ft = { "cs", "razor" },
-    dependencies = {
-      {
-        -- By loading as a dependencies, we ensure that we are available to set
-        -- the handlers for Roslyn.
-        "tris203/rzls.nvim",
-        config = true,
-      },
+    opts = {
+      -- "auto" | "roslyn" | "off"
+      --
+      -- - "auto": Does nothing for filewatching, leaving everything as default
+      -- - "roslyn": Turns off neovim filewatching which will make roslyn do the filewatching
+      -- - "off": Hack to turn off all filewatching. (Can be used if you notice performance issues)
+      filewatching = "auto",
     },
-    config = function()
-      local rzls_path = vim.fn.expand("$MASON/packages/rzls") .. "/libexec"
-
-      local cmd = {
-        "roslyn",
-        "--stdio",
-        "--logLevel=Information",
-        "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
-        "--razorSourceGenerator=" .. vim.fs.joinpath(rzls_path, "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
-        "--razorDesignTimePath=" .. vim.fs.joinpath(rzls_path, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
-        "--extension",
-        vim.fs.joinpath(rzls_path, "RazorExtension", "Microsoft.VisualStudioCode.RazorExtension.dll"),
-      }
-
-      vim.lsp.config("roslyn", {
-        cmd = cmd,
-        handlers = require("rzls.roslyn_handlers"),
-        settings = {
-          ["csharp|inlay_hints"] = {
-            csharp_enable_inlay_hints_for_implicit_object_creation = true,
-            csharp_enable_inlay_hints_for_implicit_variable_types = true,
-            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-            csharp_enable_inlay_hints_for_types = true,
-            dotnet_enable_inlay_hints_for_indexer_parameters = true,
-            dotnet_enable_inlay_hints_for_literal_parameters = true,
-            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-            dotnet_enable_inlay_hints_for_other_parameters = true,
-            dotnet_enable_inlay_hints_for_parameters = true,
-            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-          },
-          ["csharp|code_lens"] = {
-            dotnet_enable_references_code_lens = true,
-          },
-        },
-      })
-      vim.lsp.enable("roslyn")
-    end,
-    init = function()
-      -- We add the Razor file types before the plugin loads.
-      vim.filetype.add({
-        extension = {
-          razor = "razor",
-          cshtml = "razor",
-        },
-      })
-    end,
+    -- config = function()
+    --   vim.lsp.config("roslyn", {
+    --     settings = {
+    --       ["csharp|inlay_hints"] = {
+    --         csharp_enable_inlay_hints_for_implicit_object_creation = true,
+    --         csharp_enable_inlay_hints_for_implicit_variable_types = true,
+    --         csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+    --         csharp_enable_inlay_hints_for_types = true,
+    --         dotnet_enable_inlay_hints_for_indexer_parameters = true,
+    --         dotnet_enable_inlay_hints_for_literal_parameters = true,
+    --         dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+    --         dotnet_enable_inlay_hints_for_other_parameters = true,
+    --         dotnet_enable_inlay_hints_for_parameters = true,
+    --         dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+    --         dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+    --         dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+    --       },
+    --       ["csharp|code_lens"] = {
+    --         dotnet_enable_references_code_lens = true,
+    --       },
+    --     },
+    --   })
+    -- end,
   },
   {
     "folke/trouble.nvim",
