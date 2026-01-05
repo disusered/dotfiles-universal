3f6483516f0281504c20f0b5550fd3fce33051b0
Carlos Rosquillas <crosquillas@gmail.com>
2026-01-05 09:54:37 -0800

    fix(neovim): Don't use expanded LSP signature with Roslyn

--- editors/neovim/plugins/lsp-signature.lua
+++ editors/neovim/plugins/lsp-signature.lua
@@ -1,12 +1,27 @@
 return {
   "ray-x/lsp_signature.nvim",
-  event = "InsertEnter",
-  opts = {
-    bind = true,
-    floating_window = true, -- show hint in a floating window
-    hint_enable = false,    -- virtual text hint disabled
-    handler_opts = {
-      border = "single",    -- double, rounded, single, shadow, none, or a table of borders
-    },
-  },
+  event = "LspAttach",
+  config = function()
+    local cfg = {
+      bind = true,
+      floating_window = true,
+      hint_enable = false,
+      handler_opts = {
+        border = "single",
+      },
+    }
+
+    local excluded = { cs = true, razor = true }
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        local bufnr = args.buf
+        local ft = vim.bo[bufnr].filetype
+        if excluded[ft] then
+          return
+        end
+        require("lsp_signature").on_attach(cfg, bufnr)
+      end,
+    })
+  end,
 }
