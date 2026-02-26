6f78066286a8490570e6ac23597be478b96b8a07
Carlos Rosquillas <crosquillas@gmail.com>
2026-02-26 09:38:34 -0800

    feat(neovim): Make dadbod results windows taller

--- editors/neovim/plugins/dadbod.lua
+++ editors/neovim/plugins/dadbod.lua
@@ -1,18 +1,35 @@
--- This is the main plugin specification for lazy.nvim
 return {
-  "kristijanhusak/vim-dadbod-ui",
-  dependencies = {
-    { "tpope/vim-dadbod", lazy = true },
-    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    -- The init function now just calls the setup from our main module
+    init = function()
+      require("config.dadbod.main").setup()
+    end,
   },
-  cmd = {
-    "DBUI",
-    "DBUIToggle",
-    "DBUIAddConnection",
-    "DBUIFindBuffer",
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.bottom = opts.bottom or {}
+
+      -- Make output window taller by default
+      for _, win in ipairs(opts.bottom) do
+        if win.ft == "dbout" then
+          -- Merge overrides into the existing config
+          win.size = { height = 30 }
+          break
+        end
+      end
+    end,
   },
-  -- The init function now just calls the setup from our main module
-  init = function()
-    require("config.dadbod.main").setup()
-  end,
 }
