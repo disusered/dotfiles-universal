820a5261b232ae589a4391069409bb4915c6f93d
Carlos Rosquillas <crosquillas@gmail.com>
2026-03-06 10:29:52 -0800

    feat(nvim): Use cfg theme color for float borders

--- editors/neovim/plugins/catppuccin.lua
+++ editors/neovim/plugins/catppuccin.lua
@@ -6,5 +6,11 @@ return {
       transparent = true,
       solid = false,
     },
+    custom_highlights = function(C)
+      local primary = vim.fn.system("cfg theme --get secondary"):gsub("%s+", "")
+      return {
+        FloatBorder = { fg = C[primary] or C.blue, bg = C.none },
+      }
+    end,
   },
 }
