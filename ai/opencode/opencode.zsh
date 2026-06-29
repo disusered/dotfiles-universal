295ed4f6c2e3c039bcf35b21ea6318e631b0f61e
Carlos Rosquillas <crosquillas@gmail.com>
2026-06-29 15:04:46 -0700

    fix(ai): Use updated GLM key

--- ai/opencode/opencode.zsh
+++ ai/opencode/opencode.zsh
@@ -1,6 +1,6 @@
 export PATH=/home/carlos/.opencode/bin:$PATH
 
 opencode-init() {
-  op read "op://Personal/GLM Coding Plan/password" > ~/.config/opencode/.zai-key
+  op read "op://Personal/GLM Coding Plan - OpenCode/password" > ~/.config/opencode/.zai-key
   chmod 600 ~/.config/opencode/.zai-key
 }
