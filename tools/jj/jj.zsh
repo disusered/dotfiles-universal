49e4aca162add7e0b08f29aab80fd2c02ef8977d
Carlos Rosquillas <crosquillas@gmail.com>
2026-08-05 22:42:44 -0700

    feat(jj): add watchman fsmonitor and command aliases

--- tools/jj/jj.zsh
+++ tools/jj/jj.zsh
@@ -3,3 +3,14 @@ if command -v jj &>/dev/null && (( $+functions[compdef] )); then
 fi
 
 alias jj='noglob jj'
+
+# jj aliases
+# No `jb`: that is the JetBrains CLI (~/.dotnet/tools/jb).
+alias j='jj'
+alias jl='jj log'
+alias jla='jj la'
+alias js='jj st'
+alias jd='jj diff'
+alias jn='jj new'
+alias jf='jj f'
+alias jp='jj p'
