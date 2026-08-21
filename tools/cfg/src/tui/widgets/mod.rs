94d356e61e47fce8079535e4b3f18df99a8c148f
Carlos Rosquillas <crosquillas@gmail.com>
2026-08-21 16:21:02 -0700

    chore(cfg): drop duplicated crate source

--- tools/cfg/src/tui/widgets/mod.rs
+++
@@ -1,9 +0,0 @@
-mod font_preview;
-mod fuzzy_input;
-mod help;
-mod toast;
-
-pub use font_preview::FontSamples;
-pub use fuzzy_input::{FuzzyInput, FuzzyInputState};
-pub use help::HelpPopup;
-pub use toast::Toast;
